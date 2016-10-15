1310399831188725e144bb72aae0e3078943f6b2
Ssssory <itrubchaninov@list.ru>
2016-10-15 20:34:09 +0300

    ajax part delete wepon

--- ops.php
+++ ops.php
@@ -3,8 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <title>Options</title>
+    <script src="js/js-jquery-1.7.2.min.js"></script>
 </head>
 <body>
+    <script>
+        function num_id(id){             
+            temp_post = "way=delete&id="+id;
+            $.ajax({
+                    url: 'manipulate.php',
+                    type: "POST",
+                    data:  temp_post,
+                    success: function(data){
+                        alert(data);
+                    }
+                    
+        });
+    }
+        </script>
    <?php
     require_once('action.php');
     require_once('datebase.php');
@@ -24,14 +39,16 @@
             </tr>
             <?php
            foreach($person_window as $a):
+               $temp_name = $a['name'];
            ?>
             <tr>
-                <td><?php echo $a['name']?></td>
+                <td><?php echo $temp_name?></td>
                 <td><?php echo $a['hels']?></td>
                 <td><?php echo $a['damage']?></td>
-                <td><?php echo $a['id']?></td>
-                <td><a href='manipulate.php?way=edit&id=<?php $a['id']?>'>изменить</a></td>
-                <td><a href='manipulate.php?way=delete&id=<?php $a['id']?>'>удалить</a></td>
+                <td><?php echo $a['id_w']?></td>
+                <td><a href='manipulate.php?way=edit&id=<?php echo $a['id']?>'>изменить</a></td>
+                <td><a href='/war/ops.php' id_res="<?php echo $a["id"] ?>" onclick="num_id(<?php echo $a["id"] ?>) ">удалить</a></td>
+                <!--<td><a href='manipulate.php?way=delete&id=<?php echo $a['id']?>'>удалить</a></td>-->
             </tr>
             <?php endforeach ?>
         </tbody>
